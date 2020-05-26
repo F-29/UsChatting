0e6c4e22288c5dc839091a0daefceda4f03e332a
Mosich <m.chegeni7979@gmail.com>
2020-05-26 08:52:18 +0430

    refactoring for better UX

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,7 @@ socket.on('newMessage', newMessage => {
     let li = jQuery('<li style="padding: 9px; border-radius: 15px; background-color:#e0e0e0; display: inline; "></li><br><br><br>');
     li.text(`${newMessage.from.toString()}: ${newMessage.text.toString()}`);
     jQuery('#messages').append(li);
+    document.getElementById('message').innerText = "";
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -22,6 +23,7 @@ jQuery('#message-form').on('submit', function (e) {
     }, function () {
         // TODO to be done!
     });
+    document.getElementById('message').value = "";
 });
 
 socket.on('disconnect', () => {
